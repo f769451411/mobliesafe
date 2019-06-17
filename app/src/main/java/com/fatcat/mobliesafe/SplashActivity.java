2db6a0da0959bc2bfdb797e776feb44b41d9993d
hqf <hqf@qq.com>
2019-06-17 14:45:49 +0800

    day01  08

--- app/src/main/java/com/fatcat/mobliesafe/SplashActivity.java
+++ app/src/main/java/com/fatcat/mobliesafe/SplashActivity.java
@@ -8,6 +8,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fatcat.mobliesafe.util.PackageUtil;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
 
 public class SplashActivity extends Activity {
     private TextView mVersion;
@@ -16,7 +21,33 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         //requestWindowFeature(Window.FEATURE_NO_TITLE);  //代码方式去除标题
         setContentView(R.layout.activity_splash);
+        //初始化配置文件
         initView();
+        //检测apk是否更新
+        update();
+    }
+
+    private void update() {
+        HttpUtils httpUtils = new HttpUtils(2000);
+        //链接请求服务器
+        //method : 请求方式
+        //url ： 请求路径
+        //params : 请求参数   http://baidu.com/?name=ls&psw=123456
+        //RequestCallBack : 请求的回调监听
+        //ctrl+shift+x ：小转大  ctrl+shift+y ：大转小
+        String CONTENTURL = "";
+        httpUtils.send(HttpRequest.HttpMethod.GET, CONTENTURL, null, new RequestCallBack<String>() {
+            @Override
+            //请求成功返回
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+
+            }
+            //请求失败返回
+            @Override
+            public void onFailure(HttpException e, String s) {
+
+            }
+        });
     }
 
     private void initView() {
